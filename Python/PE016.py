b92efbf122505df667c69ae43aa177ac64fcd42b
Gorka Suárez <alexander.lessman@gmail.com>
2022-12-14 19:51:02 +0100

    PE problem 16 in Python

---
+++ Python/PE016.py
@@ -0,0 +1,37 @@
+"""
+Problem 16: Power digit sum
+URL: https://projecteuler.net/problem=16
+
+    2^15 = 32768 and the sum of its digits is
+    3 + 2 + 7 + 6 + 8 = 26.
+
+    What is the sum of the digits of the number 2^1000?
+"""
+
+######################################################################
+# Constants
+######################################################################
+
+GOAL = 1000
+
+
+######################################################################
+# Functions
+######################################################################
+
+def main():
+    """
+    Main entry for the problem solver.
+    """
+    result = sum([int(c) for c in str(2 ** GOAL)])
+
+    # Show the final result of the problem:
+    print(f"The sum of the digits of the number 2^{GOAL} is {result}.")
+
+
+######################################################################
+# Module execution
+######################################################################
+
+if __name__ == "__main__":
+    main()
