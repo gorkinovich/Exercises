60cf86e91e7d2046adca79849e89b2c63c459107
Gorka Suárez <alexander.lessman@gmail.com>
2022-12-31 12:34:04 +0100

    Fixing some useless error outputs

--- Erlang/build.bat
+++ Erlang/build.bat
@@ -1,5 +1,5 @@
 @echo off
-mkdir out\production\Erlang
+mkdir out\production\Erlang 2> nul
 for /r %%f in (src\*.erl) do (
     echo | set /p=">>> Compiling file: %%f"
     echo:
--- Erlang/compile.bat
+++ Erlang/compile.bat
@@ -1,4 +1,4 @@
 @echo off
-mkdir out\production\Erlang
+mkdir out\production\Erlang 2> nul
 echo | set /p=">>> Compiling file: src\%1.erl"
 erlc -o out\production\Erlang src\%1.erl
