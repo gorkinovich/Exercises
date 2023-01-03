ffde6119681442f61ad8b208aef0a7978cb144d8
Gorka Suárez <alexander.lessman@gmail.com>
2023-01-03 14:32:51 +0100

    PE problem 5 in C#

---
+++ CSharp/Euler/Extensions.cs
@@ -0,0 +1,49 @@
+﻿//======================================================================
+// Copyright (C) 2023, Gorka Suárez García
+//======================================================================
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler {
+    /// <summary>
+    /// This class represents a collection extension methods.
+    /// </summary>
+    public static class Extensions {
+        /// <summary>
+        /// Computes the product of a sequence of numeric values.
+        /// </summary>
+        /// <param name="values">The sequence of numeric values.</param>
+        /// <returns>The product of the values in the sequence</returns>
+        public static int Product (this IEnumerable<int> values) {
+            return values.Aggregate(1, (x, y) => x * y);
+        }
+
+        /// <summary>
+        /// Computes the product of a sequence of numeric values.
+        /// </summary>
+        /// <param name="values">The sequence of numeric values.</param>
+        /// <returns>The product of the values in the sequence</returns>
+        public static long Product (this IEnumerable<long> values) {
+            return values.Aggregate(1L, (x, y) => x * y);
+        }
+
+        /// <summary>
+        /// Computes the product of a sequence of numeric values.
+        /// </summary>
+        /// <param name="values">The sequence of numeric values.</param>
+        /// <returns>The product of the values in the sequence</returns>
+        public static uint Product (this IEnumerable<uint> values) {
+            return values.Aggregate(1U, (x, y) => x * y);
+        }
+
+        /// <summary>
+        /// Computes the product of a sequence of numeric values.
+        /// </summary>
+        /// <param name="values">The sequence of numeric values.</param>
+        /// <returns>The product of the values in the sequence</returns>
+        public static ulong Product (this IEnumerable<ulong> values) {
+            return values.Aggregate(1UL, (x, y) => x * y);
+        }
+    }
+}
---
+++ CSharp/Euler/PE005.cs
@@ -0,0 +1,55 @@
+﻿//======================================================================
+// Copyright (C) 2023, Gorka Suárez García
+//======================================================================
+
+/*
+ * 2520 is the smallest number that can be divided by each of the
+ * numbers from 1 to 10 without any remainder.
+ * 
+ * What is the smallest positive number that is evenly divisible
+ * (divisible with no remainder) by all of the numbers from 1 to 20?
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler {
+    /// <summary>
+    /// This class represents the problem 5.
+    /// </summary>
+    public class PE005 : IRunnable {
+        /// <summary>
+        /// Main entry for the problem solver.
+        /// </summary>
+        public void Run () {
+            const int FIRST = 1, LAST = 20;
+
+            var result = FindNumber(FIRST, LAST);
+
+            Console.Write("The smallest positive number that is a multiple ");
+            Console.WriteLine($"of all numbers from 1 to 20 is {result}.");
+        }
+
+        /// <summary>
+        /// Find the smallest natural divisible by a range of numbers.
+        /// </summary>
+        /// <param name="first">The first value of the divisors range.</param>
+        /// <param name="last">The last value of the divisors range.</param>
+        /// <returns>The smallest natural number.</returns>
+        public static ulong FindNumber (int first, int last) {
+            var divisors = Enumerable.Range(first, last)
+                                     .Select(x => (ulong) x)
+                                     .ToArray();
+            foreach (var size in Enumerable.Range(1, divisors.Length)) {
+                foreach (var combination in Tools.Combinations(divisors, size)) {
+                    var number = combination.Product();
+                    if (divisors.All(divisor => number % divisor == 0)) {
+                        return number;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
